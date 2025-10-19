3fd68cb9aa3d74075b331f788655f3ea8425d8b5
MohamedTarok2002 <mohamedtarekmahran27@gmail.com>
2025-10-19 12:20:24 +0300

    Refactor HelloWorld class to HelloServlet and update index.jsp content

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,15 @@
-package hello;
+package com.nextwork.app;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from AWS CodeBuild!");
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.getWriter().write("Hello from Servlet!");
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,12 +1,6 @@
 <html>
-
-<body>
-
-<h2>Hello ITS ME Tarek</h2>
-
-<p>This is my NextWork web application working!</p>
-
-</body>
-
+  <body>
+    <h1>Hello from Nextwork Web App!</h1>
+  </body>
 </html>
 
